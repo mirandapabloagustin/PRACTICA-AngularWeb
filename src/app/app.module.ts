8bb0014a030d0858e65b2561e2e9167b8c28f2d7
Agustin Miranda <97363454+PapasConChedar@users.noreply.github.com>
2023-10-28 08:31:32 -0300

    Revert "Fix : Arreglo del Routing"
    This reverts commit 4832aefbc83062b7dbbb55c1894289ded5b50fa3.

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AuthModule } from './modules/auth/auth.module';
+import { HttpClientModule } from '@angular/common/http';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AuthModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
